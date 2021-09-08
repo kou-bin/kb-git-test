d71ec093f9c3c96f148db7c2d69bf3204a950e68
kou-bin <58876781+kou-bin@users.noreply.github.com>
2021-09-08 16:15:10 +0800

    Update GitTest.java
    pull

--- src/main/java/com/kb/git/GitTest.java
+++ src/main/java/com/kb/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello, git master test");
         System.out.println("hello, git hot test");
         System.out.println("hello, git push");
+        System.out.println("hello");
     }
 }
